10184db6b087a19eecfe284d2e53e6c827da87ed
Mariam4324 <ashkmariam@gmail.com>
2024-01-17 23:14:18 +0300

    ff

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,11 @@
+import { Main } from "../components/Registration/Main/Main";
 import "./styles/global.scss";
 
 function App() {
   return (
     <>
-      <h1>SaintBlog</h1>
+      {/* <h1>SaintBlog</h1> */}
+      <Main />
     </>
   );
 }
