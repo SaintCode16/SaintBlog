613f5e483a6e182298f2690faaa4e1768a51b416
KEKSEGG28 <144546240+KEKSEGG28@users.noreply.github.com>
2024-01-18 18:08:43 +0300

    Ra post (#21)
    * post1
    
    * mui2
    
    * previewEdit1
    
    ---------
    
    Co-authored-by: Danil <102468675+code0verdose@users.noreply.github.com>

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,8 @@
 import { Container } from "@mui/material";
 import s from "./Main.module.scss";
+import PreviewPost from "../PreviewPost/PreviewPost";
 
 export const Main = () => {
-  return (
     <main className={s.main}>
       <Container maxWidth="lg">ОСНОВНАЯ СТРАНИЦА</Container>
     </main>
