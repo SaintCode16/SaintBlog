8e21f6b57436adfbdf739857747652d8555bd35e
ForzaMax <fforzamax@gmail.com>
2024-01-18 17:55:40 +0300

    add skeleton

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,22 @@
-import { Menu } from "@mui/material";
 import s from "./Header.module.scss";
+import { Menu } from ".././Menu";
 import Container from "@mui/material/Container";
 
 export const Header = () => {
   return (
     <header className={s.header}>
-      <Container maxWidth="lg">sdsdsd</Container>
+      <div className={s.header__top}>
+        <Container maxWidth="lg" className={s.header__container}>
+          <h1 className={s.header__title}>SaintBlog</h1>
+          <button>Регистрация</button>
+        </Container>
+      </div>
+
+      <nav className={s.nav}>
+        <Container maxWidth="lg">
+          <Menu className={s.menu} />
+        </Container>
+      </nav>
     </header>
   );
 };
