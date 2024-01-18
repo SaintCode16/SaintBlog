bee17e8f232df4f35fb12dfd5d65a3c261a41f84
KEKSEGG28 <144546240+KEKSEGG28@users.noreply.github.com>
2024-01-18 16:10:51 +0300

    Ra post (#16)
    * post1
    
    * mui2
    
    ---------
    
    Co-authored-by: Danil <102468675+code0verdose@users.noreply.github.com>

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,12 @@
+import PreviewPost from "../components/PreviewPost/PreviewPost";
+import { Post } from "../pages/Post/Post";
 import "./styles/global.scss";
 
 function App() {
   return (
     <>
       <h1>SaintBlog</h1>
+      <PreviewPost />
       {/* <Post /> */}
     </>
   );
