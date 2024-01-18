aa027b5739b0fe153aba19ba735ea259a9643ad7
Danil <mydogfromhell666@gmail.com>
2024-01-18 16:05:35 +0300

    mod: db

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,10 @@
-import { Post } from "../pages/Post/Post";
 import "./styles/global.scss";
 
 function App() {
   return (
     <>
       <h1>SaintBlog</h1>
-      <Post />
+      {/* <Post /> */}
     </>
   );
 }
