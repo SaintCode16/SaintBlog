1ad86dd106c3946a1e38f5a971bab4c6aa9d2a25
ForzaMax <fforzamax@gmail.com>
2024-01-19 12:20:01 +0300

    add main with articles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,13 @@ import PreviewPost from "../PreviewPost/PreviewPost";
 export const Main = () => {
   return (
     <main className={s.main}>
-      <Container maxWidth="lg">ОСНОВНАЯ СТРАНИЦА</Container>
+      <Container maxWidth="lg" className={s.container}>
+        <PreviewPost />
+        <PreviewPost />
+        <PreviewPost />
+        <PreviewPost />
+        <PreviewPost />
+      </Container>
     </main>
   );
 };
--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -5,7 +5,6 @@ import { Main } from "../../components/Main";
 export const Posts = () => {
   return (
     <>
-      <Main />
       <Outlet />
     </>
   );
