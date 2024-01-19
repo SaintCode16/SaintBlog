4e9f0fd9b708c8fed76118ed0b017bc90a16d1fa
Мариам <146652102+Mariam4324@users.noreply.github.com>
2024-01-19 18:37:45 +0300

    Mmm (#28)
    * commit
    
    * merge
    
    * db
    
    * db
    
    * ff
    
    * о
    
    * g
    
    * оо
    
    * ррр
    
    * gdgdf
    
    * hghf
    
    * gfh
    
    * hh
    
    * hgfhfg
    
    * form
    
    * gfgf
    
    * g
    
    * gg
    
    * дополнение к валидации
    
    * а
    
    * дополненеие к валидации
    
    ---------
    
    Co-authored-by: Mariam4324 <ashkmariam@gmail.com>
    Co-authored-by: Danil <102468675+code0verdose@users.noreply.github.com>

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
-import { Signup } from "../components/Registration/Signup/Signup";
 import "./styles/global.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./providers/router/routes";
-import { Login } from "../components/Registration/Login/Login";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
