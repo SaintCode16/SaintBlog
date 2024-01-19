3f40b5a345e1a8294dc74657355ac039d914a956
Keksegg28 <keksegg28@gmail.com>
2024-01-19 16:14:34 +0300

    edit4

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,17 @@
 import { Container } from "@mui/material";
 import s from "./Main.module.scss";
 import PreviewPost from "../PreviewPost/PreviewPost";
+import { Profile } from "../Profile/Profile";
 
 export const Main = () => {
-    <main className={s.main}>
-      <Container maxWidth="lg">ОСНОВНАЯ СТРАНИЦА</Container>
-    </main>
-  );
+  <main className={s.main}>
+    <Container maxWidth="lg" className={s.container}>
+      <Profile />
+      <PreviewPost />
+      <PreviewPost />
+      <PreviewPost />
+      <PreviewPost />
+      <PreviewPost />
+    </Container>
+  </main>;
 };
