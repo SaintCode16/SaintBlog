c223bcadcea1c7199ca86a33c909c1a6888f18c8
Elchin <130844932+50chin@users.noreply.github.com>
2024-01-19 12:32:14 +0300

    Add blog (#23)
    * add page
    
    * add pagePost
    
    ---------
    
    Co-authored-by: Danil <102468675+code0verdose@users.noreply.github.com>

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,6 @@
 import "./styles/global.scss";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./providers/router/routes";
-import { Main } from "../components/Main";
 
 function App() {
   return <RouterProvider router={router} />;
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import s from "./Input.module.scss";
 
 export const Input = () => {
