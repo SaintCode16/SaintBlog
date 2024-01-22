ae0ded9cfea17ac84f5d40549b610caa5513308f
KEKSEGG28 <144546240+KEKSEGG28@users.noreply.github.com>
2024-01-22 17:30:30 +0300

    Ra profile (#35)
    * spinner
    
    * spinner
    
    * format

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import { Api } from "./Api";
 import { useState } from "react";
 
-const [isAuth, setIsAuth] = useState(false)
+// const [isAuth, setIsAuth] = useState(false)
 
 // isAuth
 // GetUserData (API) беру из LS токен, получаю данные пользователя, записываю в store объект, если все норм isAuth = true, если нет false. При любом рендере страницы проверять isAuth
-
 
 export const store = configureStore({
   reducer: {
