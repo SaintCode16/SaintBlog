94a0093f738538fa19f09873de4397beafa6a378
50chin <50chin50@gmail.com>
2024-01-23 14:20:18 +0300

    addPage notFound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,25 @@
-import s from "./NotFound.module.scss";
+import cat from './404/cat.webp';
+import img from './404/404.webp';
+import s from './NotFound.module.scss';
+import { Link as LinkRRD } from 'react-router-dom';
+
+import { Container, Button } from '@mui/material';
 
 export const NotFound = () => {
-  return <div></div>;
+  return (
+    <div className={s.notFound}>
+      <Container maxWidth="lr">
+        <div className={s.notFound__wrapper}>
+          <img src={cat} alt="кот" className={s.notFound__cat} />
+          <div>
+            <h1 className={s.notFound__title}>Error</h1>
+            <img src={img} alt="error" className={s.notFound__error} />
+          </div>
+          <LinkRRD to={''}>
+            <Button className={s.notFound__btn}>На главную</Button>
+          </LinkRRD>
+        </div>
+      </Container>
+    </div>
+  );
 };
