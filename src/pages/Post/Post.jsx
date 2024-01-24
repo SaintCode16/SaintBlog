9d66d9eb605028f2ff75dd686372071618af82aa
ForzaMax <fforzamax@gmail.com>
2024-01-24 11:59:30 +0300

    pull!

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -99,7 +99,7 @@ export const Post = () => {
   useEffect(() => {
     if (comments && post) {
       setPostComments(
-        comments.filter((comment) => +comment.postId === post.id)
+        comments.filter((comment) => +comment.postId === post.id),
       );
     }
   }, [comments, post]);
